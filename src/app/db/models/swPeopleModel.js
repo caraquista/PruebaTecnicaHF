b06536c06b223769c7637b7e55b3080dd35b74a1
Daniel Diaz <caraquist@gmail.com>
2023-08-31 01:27:24 +0200

    refactor (middleware) se actualizo el codigo para que los objetos de acceso a la data no dieran errorer de linter

--- src/app/db/models/swPeopleModel.js
+++ src/app/db/models/swPeopleModel.js
@@ -2,14 +2,14 @@
 
 module.exports = (sequelize, DataTypes) => {
     const swPeople = sequelize.define(
-        'swPeople',{
+        'swPeople', {
             id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
             name: DataTypes.STRING,
             mass: DataTypes.INTEGER,
             height: DataTypes.INTEGER,
             homeworld_name: DataTypes.STRING,
             homeworld_id: DataTypes.STRING,
-        }, 
+        },
         {
             paranoid: true
         }
--- src/app/db/models/swPlanetModel.js
+++ src/app/db/models/swPlanetModel.js
@@ -2,11 +2,11 @@
 
 module.exports = (sequelize, DataTypes) => {
     const swPlanet = sequelize.define(
-        'swPlanet',{
+        'swPlanet', {
             id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
             name: DataTypes.STRING,
             gravity: DataTypes.FLOAT,
-        }, 
+        },
         {
             paranoid: true
         }
