4f32b312345d92bdf26760bada3707a4dcb57f16
Daniel Diaz <caraquist@gmail.com>
2023-08-31 01:56:10 +0200

    feat (config) Se agrego un manejador basico para las variables de entorno

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  url_external_service: process.env.URL_EXTERNAL_SERVICE,
+  prefix_person: process.env.PREFIX_PERSON,
+  prefix_planet: process.env.PREFIX_PLANET,
+  suffix_wookie: process.env.SUFFIX_WOOKIE
+};
