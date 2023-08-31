2ed32edf7d0177183e80c0fd1d63ef8bfe73996a
Daniel Diaz <caraquist@gmail.com>
2023-08-31 03:22:09 +0200

    refactor (getPlanet) se realizaron los cambios necesarios para que el servicio de planetas regrese la data como se espera, se agrego un manejo de errores basico para que el servidor no se caiga en ciertas cisrcunstancias

--- src/app/Planet/Planet.js
+++ src/app/Planet/Planet.js
@@ -1,11 +1,38 @@
+const config = require('../../config');
+
 class Planet {
-    constructor(id) {
+    constructor(id, app) {
         this.id = id;
+        this.app = app;
     }
 
-    async init(){
-        throw new Error('To be implemented');
-    }
+    async init() {
+        let crearRegistro = false;
+        let planet = await this.app.db.swPlanet.findOne({
+          where: { id: this.id },
+        });
+
+        if (planet === null) {
+            crearRegistro = true;
+            planet = await this.app.swapiFunctions.genericRequest(
+                `${config.url_external_service}${config.prefix_planet}${this.id}`,
+                'GET',
+                null
+            );
+            this.gravity = parseFloat(planet.gravity.replace(/[^\d.]*/g, '')) || 0.0;
+        } else {
+            this.gravity = planet.gravity;
+        }
+        this.name = planet.name;
+
+        if (crearRegistro) {
+            await this.app.db.swPlanet.create({
+                id: this.id,
+                name: this.name,
+                gravity: this.gravity
+            });
+        }
+      }
 
     getName() {
         return this.name;
@@ -15,3 +42,5 @@ class Planet {
         return this.gravity;
     }
 }
+
+exports.Planet = Planet;
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  logging_active: process.env.LOGGING_ACTIVE,
   url_external_service: process.env.URL_EXTERNAL_SERVICE,
   prefix_person: process.env.PREFIX_PERSON,
   prefix_planet: process.env.PREFIX_PLANET,
